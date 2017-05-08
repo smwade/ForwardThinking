a73ece468d3b7d11e81a0ed6686a789f8af3ac55
Sean Wade <seanwade@lovelandinnovations.com>
2017-05-08 17:19:06 -0600

    work work work:

--- tests/test_pass_forward.py
+++ tests/test_pass_forward.py
@@ -1,10 +1,10 @@
 from forwardThinking.datasets import load_mnist
-from forwardThinking.models import AdaptiveForwardThinking
+from forwardThinking.models import PassForwardThinking, AdaptiveForwardThinking
 
 x_train, y_train, x_test, y_test = load_mnist()
 
-#model = PassForwardThinking([784, 100, 100, 10], freeze=False)
-#model.fit(x_train, y_train, x_test, y_test, epochs=100)
+model = PassForwardThinking([784, 100, 100, 10], freeze=True)
+model.fit(x_train, y_train, x_test, y_test, epochs=2)
 
-model = AdaptiveForwardThinking(784, 1, 10, freeze=False)
-model.fit(x_train, y_train, x_test, y_test, max_epochs=100)
+#model = AdaptiveForwardThinking(784, 1, 10, freeze=True)
+#model.fit(x_train, y_train, x_test, y_test, max_epochs=100)
